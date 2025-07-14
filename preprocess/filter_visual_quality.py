2fc948dc8dd5b50a2ed34b50abbc2ccfc6c2de06
eme <edgar.medina@qualityminds.de>
2025-07-15 01:06:11 +0200

    bugfix: a common PyTorch multiprocessing issue :/

--- preprocess/filter_visual_quality.py
+++ preprocess/filter_visual_quality.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# export PYTORCH_MP_START_METHOD=spawn or:
+import multiprocessing as mp
+mp.set_start_method('spawn', force=True)
 
 import os
 import tqdm
